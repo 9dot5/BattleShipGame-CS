960f60769d73ed060ce95e001942972225f92fc0
aga_rsilva <rui.silva_ext@a-to-be.com>
2024-05-22 14:48:20 +0100

    Initial state of project
    Change-Id: I092320d19796d41e70a2d4695335cfd9c9ce4bde

---
+++ Battleship.py
@@ -0,0 +1,125 @@
+board = [[0 for _ in range(10)] for _ in range(10)] 
+
+
+for x in [5, 4, 3, 2, 2, 1, 1]:
+
+    invalid_move = 1
+
+    while invalid_move:
+        invalid_move = 0
+
+        if x == 5:
+            print("Carrier position:")
+        if x == 4:
+            print("Battleship position:")
+        if x == 3:
+            print("Destroyer position:")
+        if x == 2:
+            print("Cruiser position:")
+        if x == 1:
+            print("Submarine position:")
+
+        linha= input("linha:")
+        
+        while int(linha) > 10 or int(linha) < 1:
+            print("posicao invalida\n")
+            linha= input("linha:")
+            continue
+
+        coluna= input("coluna:")
+
+        while int(coluna) > 10 or int(coluna) < 1:
+            print("posicao invalida\n")
+            coluna= input("coluna:")
+            continue
+
+        linhas = int(linha) - 1
+        colunas = int(coluna) - 1
+
+
+
+        orientacao = input("orientação (u, d, l, r)")
+
+        while orientacao not in ['u', 'd', 'l', 'r']:
+            print("direcção invalida\n")
+            orientacao = input("orientação (u, d, l, r)")
+            continue
+
+        if orientacao == 'u' and int(linhas) < x-1:
+            print("posicao invalida\n")
+            invalid_move=1
+            continue
+
+
+        if orientacao == 'd' and int(linhas) > 10-x:
+            print("posicao invalida\n")
+            invalid_move=1
+            continue
+
+
+        if orientacao == 'l' and int(colunas) < x-1:
+            print("posicao invalida\n")
+            invalid_move=1
+            continue
+
+
+        if orientacao == 'r' and int(colunas) > 10-x:
+            print("posicao invalida\n")
+            invalid_move=1
+            continue
+        
+        for y in range(x):
+            if orientacao == 'u':
+                if board[int(linhas) - y][int(colunas)] == 1:
+                    print("posicao invalida\n")
+                    invalid_move=1
+                    break
+
+            if orientacao == 'd':
+                if board[int(linhas)+ y][int(colunas) ] == 1:
+                    print("posicao invalida\n")
+                    invalid_move=1
+                    break
+
+            if orientacao == 'l':
+                if board[int(linhas)][int(colunas) - y ] == 1:
+                    print("posicao invalida\n")
+                    invalid_move=1
+                    break
+
+            if orientacao == 'r':
+                if board[int(linhas)][int(colunas) + y] == 1:
+                    print("posicao invalida\n")
+                    invalid_move=1
+                    break
+
+
+
+    for y in range(x):
+        if orientacao == 'u':
+            board[int(linhas) - y][int(colunas)] = 1
+        if orientacao == 'd':
+            board[int(linhas)+ y][int(colunas) ] = 1
+        if orientacao == 'l':
+            board[int(linhas) ][int(colunas) - y] = 1
+        if orientacao == 'r':
+            board[int(linhas)][int(colunas) +y ] = 1
+
+
+
+
+
+board_vec = []
+
+for row in board:
+    for num in row:
+        board_vec.append(int(num)) 
+
+print("board:")
+for row in  board:
+    print(row)
+
+print("\n")
+print("board vector:")
+
+print(board_vec)
